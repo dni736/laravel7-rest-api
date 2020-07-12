b928be5d2c551b6a1ff29538203c6aea58806f59
TutsPrime <tutsprimedotcom@gmail.com>
2020-07-12 09:19:12 +0700

    Learn basic Eloquent Relationship

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'category_id'];
+	protected $fillable = ['name', 'price', 'category_id'];
+	
+	public function category()
+	{
+		return $this->belongsTo(Category::class);
+	}
 }
 
 /*
