a65db02a579a781af57dd46d6e87983b715ee745
TutsPrime <tutsprimedotcom@gmail.com>
2020-07-19 07:07:20 +0700

    Learn controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Resources\ProductResource;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::orderBy('name')->get();
+        return ProductResource::collection($products);
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    public function store()
+    {
+        // validation goes here
+        $data = request()->all();
+
+        $product = Product::create($data); 
+
+        return new ProductResource($product);
+    }
+    
+    public function update(Product $product)
+    {
+        // validation goes here
+        $data = request()->all();
+
+        $product->update($data); 
+
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->noContent();
+    }
+}
